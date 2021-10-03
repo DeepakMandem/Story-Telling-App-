dffeab3142d7f91f7880a824c9ae9131a6490eff
DeepakMandem <72325190+DeepakMandem@users.noreply.github.com>
2021-10-02 19:55:51 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDfU7KJzCDH1YsbuLF3KPhcEFl-2_R5aEI",
+    authDomain: "story-telling-app-63c06.firebaseapp.com",
+    databaseURL: "https://story-telling-app-63c06-default-rtdb.firebaseio.com",
+    projectId: "story-telling-app-63c06",
+    storageBucket: "story-telling-app-63c06.appspot.com",
+    messagingSenderId: "966294274313",
+    appId: "1:966294274313:web:986b64fadb059ce179d3d8"
+  };
